cd03810ea29da659bcc4c1e55d9a6a840c14cf6d
DanielPanjos <pombo.daniel@gmail.com>
2020-09-22 16:02:33 -0300

    Upgrade code

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -31,58 +31,59 @@ class App{
   }
 
   addrespository(event){
-  event.preventDefault();
-  
-  let inputP = this.inputPeso.value;
-  let inputA = this.inputAltura.value;
-  let nome = this.inputNome.value;
-  
-  
-  
-  this.setLoading();
-  
- // this.listaEl.innerHTML="";
-  
-  const imc = (inputP, inputA) => {
-  
-      const resultado = (inputP / (inputA * inputA)).toFixed(2);
-      let grau = ''; 
-  
-      if (resultado < 18.5) {
-        grau = 'Magreza';
-      } else if (resultado < 25) {
-        grau = 'Normal';
-      } else if (resultado < 30) {
-       // console.log(`${resultado} Sobrepeso - Grau 1`);
-        grau = 'sobrepeso - Grau 1';
-      } else if (resultado < 40) {
-       // console.log(`${resultado} Obesidade - Grau 2`);
-        grau = 'Obesidade - Grau 2';
-      } else {
-        //console.log(`${resultado} Obesidade Grave - Grau 3`);
-        grau = 'Obesidade Grave - Grau 3';
-      }
-  
-this.setLoading(false);
-this.inputPeso.value = '';
-this.inputAltura.value = '';
-this.inputNome.value ='';
+    event.preventDefault();
     
+    let inputP = this.inputPeso.value;
+    let inputA = this.inputAltura.value;
+    let nome = this.inputNome.value;
+    
+    inputA = inputA.replace(",", ".");
+    console.log(inputA);
+    
+    this.setLoading();
+    
+  // this.listaEl.innerHTML="";
+    
+    const imc = (inputP, inputA) => {
+    
+        const resultado = (inputP / (inputA * inputA)).toFixed(2);
+        let grau = ''; 
+    
+        if (resultado < 18.5) {
+          grau = 'Magreza';
+        } else if (resultado < 25) {
+          grau = 'Normal';
+        } else if (resultado < 30) {
+        // console.log(`${resultado} Sobrepeso - Grau 1`);
+          grau = 'sobrepeso - Grau 1';
+        } else if (resultado < 40) {
+        // console.log(`${resultado} Obesidade - Grau 2`);
+          grau = 'Obesidade - Grau 2';
+        } else {
+          //console.log(`${resultado} Obesidade Grave - Grau 3`);
+          grau = 'Obesidade Grave - Grau 3';
+        }
+    
+  this.setLoading(false);
+  this.inputPeso.value = '';
+  this.inputAltura.value = '';
+  this.inputNome.value ='';
       
-      this.usuario.push({
-        nome,
-        resultado,
-        grau,
-      });
-      //console.log(this.usuario);
-      let itemList = document.createElement('li');
-      itemList.appendChild(document.createTextNode(`${nome}, `));
-      itemList.appendChild(document.createTextNode(`${resultado} - `));
-      itemList.appendChild(document.createTextNode(grau));
+        
+        this.usuario.push({
+          nome,
+          resultado,
+          grau,
+        });
+        //console.log(this.usuario);
+        let itemList = document.createElement('li');
+        itemList.appendChild(document.createTextNode(`${nome}, `));
+        itemList.appendChild(document.createTextNode(`${resultado} - `));
+        itemList.appendChild(document.createTextNode(grau));
 
-      this.listaEl.appendChild(itemList);
-};
-setTimeout(() => imc(inputP, inputA), 2000);
+        this.listaEl.appendChild(itemList);
+  };
+  setTimeout(() => imc(inputP, inputA), 2000);
 }
 }
 
--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ server.use(express.static("public"))
 
 
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    res.sendFile(__dirname + "/views/Index.html")
 })
 
 
